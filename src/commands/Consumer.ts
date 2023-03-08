f1d1d80c450d24a6194b840429b4ed1b522fab23
David Suárez <dsuarez@comparaonline.com>
2023-03-08 11:33:38 -0600

    fix: prevent to consume same topic twice

--- src/commands/Consumer.ts
+++ src/commands/Consumer.ts
@@ -2,8 +2,9 @@ import Kafka from "../helpers/Kafka";
 
 export default async function (topics: string[]) {
   const kafka = new Kafka();
+  const topicsFiltered = [...new Set(topics.map((t) => t.trim()))];
 
-  await kafka.consume(topics, (topic, data) => {
+  await kafka.consume(topicsFiltered, (topic, data) => {
     console.log(`From ${topic}:`, data);
   });
 }
