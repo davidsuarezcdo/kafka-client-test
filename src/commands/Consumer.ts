db423dad6a5660a38a13f18ed85bd412839c2890
David Suárez <dsuarez@comparaonline.com>
2023-08-31 15:17:25 +0000

    feat: colorize data

--- src/commands/Consumer.ts
+++ src/commands/Consumer.ts
@@ -1,10 +1,12 @@
 import Kafka from "../helpers/Kafka";
+import { objectToJsonColorized } from "../helpers/jsonColorize";
 
 export default async function (topics: string[]) {
   const kafka = new Kafka();
   const topicsFiltered = [...new Set(topics.map((t) => t.trim()))];
 
   await kafka.consume(topicsFiltered, (topic, data) => {
-    console.log(`From ${topic}:`, data);
+    console.log(`From ${topic}:`);
+    console.log(objectToJsonColorized(data));
   });
 }
--- src/helpers/jsonColorize.ts
+++ src/helpers/jsonColorize.ts
@@ -3,12 +3,8 @@ import chalk from "chalk";
 export const objectToJsonColorized = (json: Record<any, any>) => {
   const jsonString = JSON.stringify(json, null, 2);
 
-  console.log(
-    jsonString
-      .replace(/"(.*?)":/g, chalk.green('"$1":'))
-      .replace(/: ".*?"/g, chalk.cyan(': "$&"'))
-      .replace(/: (\d+)/g, chalk.magenta(": $1"))
-  );
-
-  return jsonString;
+  return jsonString
+    .replace(/"(.*?)":/g, chalk.green('"$1":'))
+    .replace(/: ".*?"/g, chalk.cyan(': "$&"'))
+    .replace(/: (\d+)/g, chalk.magenta(": $1"));
 };
